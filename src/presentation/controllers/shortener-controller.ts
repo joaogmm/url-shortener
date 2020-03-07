6e94596dd1eb035a27c53c778e7e9dda5c6ecd34
Joao Gabriel de Morais Magalhaes <joaomagalhaes@ibm.com>
2020-03-07 18:23:04 -0300

    refactor: make ShortenerController implements a interface

--- src/presentation/controllers/shortener-controller.ts
+++ src/presentation/controllers/shortener-controller.ts
@@ -1,13 +1,12 @@
 import { HttpResponse, HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/http-helper'
+import { Controller } from '../protocols/controller'
 
-export class ShortenerController {
+export class ShortenerController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.url) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('url')
-      }
+      return badRequest(new MissingParamError('url'))
     }
   }
 }
