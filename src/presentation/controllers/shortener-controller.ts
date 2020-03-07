b52a9a65add9c36ce48cb2fa37ddc21b74930aa7
Joao Gabriel de Morais Magalhaes <joaomagalhaes@ibm.com>
2020-03-07 18:13:51 -0300

    refactor: add customized error for missing param

--- src/presentation/controllers/shortener-controller.ts
+++ src/presentation/controllers/shortener-controller.ts
@@ -1,11 +1,12 @@
 import { HttpResponse, HttpRequest } from '../protocols/http'
+import { MissingParamError } from '../errors/missing-param-error'
 
 export class ShortenerController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.url) {
       return {
         statusCode: 400,
-        body: new Error('Missing param: url')
+        body: new MissingParamError('url')
       }
     }
   }
