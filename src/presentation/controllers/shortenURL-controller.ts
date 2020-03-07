9a2424b0f7fefd609e80b24851086d2795325189
Joao Gabriel de Morais Magalhaes <joaomagalhaes@ibm.com>
2020-03-07 20:28:55 -0300

    feat: ensure ShortenURLController returns 400 if an invalid url is provided

--- src/presentation/controllers/shortenURL-controller.ts
+++ src/presentation/controllers/shortenURL-controller.ts
@@ -2,11 +2,21 @@ import { HttpResponse, HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
+import { URLValidator } from '../protocols/url-validator'
+import { InvalidParamError } from '../errors/invalid-param-error'
 
-export class ShortenerController implements Controller {
+export class ShortenURLController implements Controller {
+  constructor (private readonly urlValidator: URLValidator) {
+    this.urlValidator = urlValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.url) {
       return badRequest(new MissingParamError('url'))
+    }
+    const isValid = this.urlValidator.isValid(httpRequest.body.url)
+    if (!isValid) {
+      return badRequest(new InvalidParamError('url'))
     }
   }
 }
---
+++ src/presentation/protocols/url-validator.ts
@@ -0,0 +1,3 @@
+export interface URLValidator {
+  isValid (email: string): boolean
+}
