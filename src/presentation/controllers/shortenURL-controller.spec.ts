03f6234ea6abf99d6ce1766bc13411aad89c73eb
Joao Gabriel de Morais Magalhaes <joaomagalhaes@ibm.com>
2020-03-07 20:29:38 -0300

    test: ensure ShortenURLController returns 400 if an invalid url is provided

--- src/presentation/controllers/shortenURL-controller.spec.ts
+++ src/presentation/controllers/shortenURL-controller.spec.ts
@@ -1,12 +1,48 @@
-import { ShortenerController } from './shortenURL-controller'
+import { ShortenURLController } from './shortenURL-controller'
 import { MissingParamError } from '../errors/missing-param-error'
+import { URLValidator } from '../protocols/url-validator'
+import { InvalidParamError } from '../errors/invalid-param-error'
+
+interface SutTypes {
+  sut: ShortenURLController
+  urlValidatorStub: URLValidator
+}
+
+const makeSut = (): SutTypes => {
+  class URLValidatorStub implements URLValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  const urlValidatorStub = new URLValidatorStub()
+  const sut = new ShortenURLController(urlValidatorStub)
+  return {
+    sut,
+    urlValidatorStub
+  }
+}
 
 describe('ShortenURL Controller', () => {
   test('Should return 400 if no url is provided', () => {
-    const sut = new ShortenerController()
-    const httpRequest = { body: {} }
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: { }
+    }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('url'))
+  })
+
+  test('Should return 400 if an invalid url is provided', () => {
+    const { sut, urlValidatorStub } = makeSut()
+    jest.spyOn(urlValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const httpRequest = {
+      body: {
+        url: 'invalid_url'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('url'))
   })
 })
