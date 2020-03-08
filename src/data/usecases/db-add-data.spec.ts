956b05948ddf53e5d7a19cfd593eba424fea7c51
Joao Gabriel de Morais Magalhaes <joaomagalhaes@ibm.com>
2020-03-08 14:41:45 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/db-add-data.spec.ts
+++ src/data/usecases/db-add-data.spec.ts
@@ -1,15 +1,28 @@
 import { DbAddData } from './db-add-data'
 import { HashGenerator } from '../protocols/hashGenerator'
 
+interface SutTypes {
+  sut: DbAddData
+  hashGeneratorStub: HashGenerator
+}
+
+const makeSut = (): SutTypes => {
+  class HashGeneratorStub implements HashGenerator {
+    async genHash (hashSize: number): Promise<string> {
+      return new Promise(resolve => resolve('hashed_password'))
+    }
+  }
+  const hashGeneratorStub = new HashGeneratorStub()
+  const sut = new DbAddData(hashGeneratorStub)
+  return {
+    sut,
+    hashGeneratorStub
+  }
+}
+
 describe('DbAddData usecase', () => {
   test('Should return call hashGenerator with correct value', async () => {
-    class HashGeneratorStub implements HashGenerator {
-      async genHash (hashSize: number): Promise<string> {
-        return new Promise(resolve => resolve('hashed_password'))
-      }
-    }
-    const hashGeneratorStub = new HashGeneratorStub()
-    const sut = new DbAddData(hashGeneratorStub)
+    const { sut, hashGeneratorStub } = makeSut()
     const hashGenSpy = jest.spyOn(hashGeneratorStub, 'genHash')
     const data = {
       url: 'www.google.com'
