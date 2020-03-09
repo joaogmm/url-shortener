a8edccd0d1ced1d11d4957fca679fabbd78950da
Joao Gabriel de Morais Magalhaes <joaomagalhaes@ibm.com>
2020-03-08 21:08:45 -0300

    feat: ensure DataMongoRepository returna dataModel on success

--- src/domain/models/input-data.ts
+++
@@ -1,3 +0,0 @@
-export interface InputDataModel {
-  url: string
-}
---
+++ src/infra/db/mongodb/data-repository/data.ts
@@ -0,0 +1,13 @@
+import { DataModel } from '../../../../data/models/add-url'
+import { AddDataRepository } from '../../../../data/protocols/add-data-repository'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class DataMongoRepository implements AddDataRepository {
+  async add (data: DataModel): Promise<DataModel> {
+    const dataCollection = await MongoHelper.getCollection('urls')
+    const result = await dataCollection.insertOne(data)
+    const urls = result.ops[0]
+    console.log(urls)
+    return urls
+  }
+}
