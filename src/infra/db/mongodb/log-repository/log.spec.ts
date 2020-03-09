948b3102e45944b44d5e2a850587d79c847611a9
Joao Gabriel de Morais Magalhaes <joaomagalhaes@ibm.com>
2020-03-08 21:10:04 -0300

    test: ensure DataMongoRepository returna dataModel on success

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,31 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import { LogMongoRepository } from './log'
+import { LogErrorRepository } from '../../../../data/protocols/db/log-error-repository'
+
+const makeSut = (): LogErrorRepository => {
+  return new LogMongoRepository()
+}
+
+describe('Log Mongo Repository', () => {
+  let errorCollection: Collection
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    errorCollection = await MongoHelper.getCollection('errors')
+    await errorCollection.deleteMany({})
+  })
+
+  test('Should create an error log on sucess', async () => {
+    const sut = makeSut()
+    await sut.logError('any_error')
+    const count = await errorCollection.countDocuments()
+    expect(count).toBe(1)
+  })
+})
