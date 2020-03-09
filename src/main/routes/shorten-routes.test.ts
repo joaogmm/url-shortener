92b39ac40d130830744eed901873dee023a7d314
Joao Gabriel de Morais Magalhaes <joaomagalhaes@ibm.com>
2020-03-09 00:14:09 -0300

    refactor: connect to mongodb befora tetss

--- src/main/routes/shorten-routes.test.ts
+++ src/main/routes/shorten-routes.test.ts
@@ -1,7 +1,23 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+
+let accountCollection: Collection
 
 describe('Shorten Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('urls')
+    await accountCollection.deleteMany({})
+  })
   test('Should return 200', async () => {
     await request(app)
       .post('/curtin/enshort')
