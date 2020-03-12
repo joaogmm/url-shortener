a35df7dd4afab6f60941a0202e612be7bcfbca06
Joao Gabriel de Morais Magalhaes <joaomagalhaes@ibm.com>
2020-03-11 23:52:04 -0300

    test: ensure all the routes works correctly

--- src/main/routes/shorten-routes.test.ts
+++ src/main/routes/shorten-routes.test.ts
@@ -3,20 +3,18 @@ import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { Collection } from 'mongodb'
 
-let accountCollection: Collection
+let dataCollection: Collection
 
 describe('Shorten Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
-
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
-
   beforeEach(async () => {
-    accountCollection = await MongoHelper.getCollection('urls')
-    await accountCollection.deleteMany({})
+    dataCollection = await MongoHelper.getCollection('urls')
+    await dataCollection.deleteMany({})
   })
   test('Should return 200 on success', async () => {
     await request(app)
@@ -25,5 +23,55 @@ describe('Shorten Routes', () => {
         url: 'www.google.com'
       })
       .expect(200)
+  })
+  test('Should return 404 on delete failure', async () => {
+    dataCollection = await MongoHelper.getCollection('urls')
+    await request(app)
+      .delete('/delete/www.curtin.com/:hash')
+      .expect(404)
+  })
+  test('Should return 204 on deletion success', async () => {
+    dataCollection = await MongoHelper.getCollection('urls')
+    await dataCollection.insertOne({
+      url: 'www.google.com',
+      shortedUrl: 'any_hash',
+      _id: 123456
+    })
+    await request(app)
+      .delete('/delete/www.curtin.com/' + 'any_hash')
+      .expect(204)
+  })
+  test('Should return 204 on retrieve success', async () => {
+    dataCollection = await MongoHelper.getCollection('urls')
+    await dataCollection.insertOne({
+      url: 'www.google.com',
+      shortedUrl: 'any_hash',
+      _id: 123456
+    })
+    const httpResponse = await request(app)
+      .get('/retrieve/www.curtin.com/' + 'any_hash')
+      .expect(200)
+    expect(httpResponse.body.url).toEqual('www.google.com')
+  })
+  test('Should return 404 on retrieve fails', async () => {
+    await request(app)
+      .get('/retrieve/www.curtin.com/' + 'any_hash')
+      .expect(404)
+  })
+  test('Should return 302 on redirect success', async () => {
+    dataCollection = await MongoHelper.getCollection('urls')
+    await dataCollection.insertOne({
+      url: 'www.google.com',
+      shortedUrl: 'any_hash',
+      _id: 123456
+    })
+    await request(app)
+      .get('/' + 'any_hash')
+      .expect(302) // 302 - Page Found
+  })
+  test('Should return 404 on retrieve fails', async () => {
+    await request(app)
+      .get('/' + 'any_hash')
+      .expect(404)
   })
 })
