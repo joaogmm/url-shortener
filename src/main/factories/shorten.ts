e5ada4608f5ad45d3d9bbceaf52647fa79dc19b1
Joao Gabriel de Morais Magalhaes <joaomagalhaes@ibm.com>
2020-03-12 01:25:17 -0300

    feat: adding decorator with ServerError capturing and store to db

--- src/main/factories/shorten.ts
+++ src/main/factories/shorten.ts
@@ -3,12 +3,24 @@ import { URLValidatorAdapter } from '../../utils/url-validator-adapter'
 import { DbAddData } from '../../data/usecases/db-add-data'
 import { CryptoAdapter } from '../../infra/cryptography/crypto/crypto-adapter'
 import { DataMongoRepository } from '../../infra/db/mongodb/data-repository/data'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
+import { LogControllerDecorator } from '../decorator/log'
+import { Controller } from '../../presentation/protocols/controller'
 
-export const makeShortenController = (): ShortenURLController => {
+export const makeShortenController = (): Controller => {
   const encryptation = ('md5')
+  // Adapters
   const urlValidatorAdapter = new URLValidatorAdapter()
   const cryptoAdapter = new CryptoAdapter(encryptation)
+
+  // Data
   const dataMongoRepository = new DataMongoRepository()
   const dbAddData = new DbAddData(dataMongoRepository)
-  return new ShortenURLController(urlValidatorAdapter, dbAddData, cryptoAdapter)
+
+  // Build the controller
+  const shortenURLController = new ShortenURLController(urlValidatorAdapter, dbAddData, cryptoAdapter)
+
+  // Log
+  const logMongoRepository = new LogMongoRepository()
+  return new LogControllerDecorator(shortenURLController, logMongoRepository)
 }
