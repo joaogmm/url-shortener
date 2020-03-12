1e3d75fa4932de553f0a68afde33e2843c17da1e
Joao Gabriel de Morais Magalhaes <joaomagalhaes@ibm.com>
2020-03-11 21:59:06 -0300

    feat: add retrieve URL function

---
+++ src/presentation/controllers/redirect/retrieveURL-controller.ts
@@ -0,0 +1,28 @@
+import { MissingParamError } from '../../errors/missing-param-error'
+import { badRequest, serverError, ok, notFound } from '../../helpers/http-helper'
+import { RetrieveData } from '../../../domain/usescases/retrieve-data'
+import { Controller } from '../../protocols/controller'
+import { HttpRequest, HttpResponse } from '../../protocols/http'
+import { NotFoundParamError } from '../../errors/notfound-param-error'
+
+export class RetrieveURLController implements Controller {
+  constructor (private readonly retrieveData: RetrieveData) {
+  }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      if (!httpRequest.params.shortUrl) {
+        return badRequest(new MissingParamError('URL'))
+      }
+      const url = await this.retrieveData.retrieve(httpRequest.params.shortUrl)
+      console.log(url)
+      if (typeof url === 'undefined' || url === null) {
+        return notFound(new NotFoundParamError(httpRequest.params.shortUrl))
+      }
+      return ok({ url })
+    } catch (error) {
+      console.log(error)
+      return serverError(error)
+    }
+  }
+}
