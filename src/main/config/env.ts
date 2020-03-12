c2d704f27ff3edcf985e70276956a327c1e24262
Joao Gabriel de Morais Magalhaes <joaomagalhaes@ibm.com>
2020-03-11 22:13:57 -0300

    refactor: remove wrong reference from env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,4 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/url-shortener',
-  port: process.env.PORT || 5050,
-  jwtSecret: process.env.JWT_SECRET || 'tjg70==5H'
+  port: process.env.PORT || 5050
 }
