982db22b80307cc2b0460fc6f4ecc631f9e17b6c
Joao Gabriel de Morais Magalhaes <joaomagalhaes@ibm.com>
2020-03-13 00:41:05 -0300

    refactor: docker port

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,4 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/url-shortener',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/url-shortener',
   port: process.env.PORT || 5050
 }
